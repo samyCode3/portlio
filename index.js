bb49a422ee094f848d4e5396bc6db21d0fc95ef9
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-06-24 10:14:27 +0100

    update

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let about = document.querySelector("#about");
+let icon = document.querySelector("#icon");
+let iconTarget = document.querySelector("#icon-target");
 let aboutPort = document.querySelector("#about-port");
 
 about.addEventListener("click", (e) =>  
 {
      e.preventDefault();  
      aboutPort.scrollIntoView();
-     console.log("hdbjdb")
+     
+})
+icon.addEventListener("click", () => {
+     iconTarget.setAttribute("style", "display: block");
+     icon.setAttribute("style", "display: none");
+     console.log(iconTarget)
 })
