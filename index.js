b5cfbeca5526970277c8fb2cd06ce68769758c3c
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-08-24 00:33:48 +0100

    update

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
-let about = document.querySelector("#about");
-let icon = document.querySelector("#icon");
-let iconTarget = document.querySelector("#icon-target");
-let aboutPort = document.querySelector("#about-port");
+const slider = document.getElementById('image-slider')
+const images = ['3d-interlocked-hands-four-people-27283315.jpg',
+'code1.png', 'code2.png', 'code3.png', 'code4.png'
+]
 
-about.addEventListener("click", (e) =>  
+let i = 0;
+function prev()
 {
-     e.preventDefault();  
-     aboutPort.scrollIntoView();
-     
-})
-icon.addEventListener("click", () => {
-     iconTarget.setAttribute("style", "display: block");
-     icon.setAttribute("style", "display: none");
-     console.log(iconTarget)
-})
+     if(i <= 0) i = images.length
+     i--
+     return setImg();
+}
+
+function next()
+{
+ if(i >= images.length-1) i = -1;
+ i++ 
+ return setImg()
+}
+
+function setImg( )
+{
+     return slider.setAttribute('src', `./${images[i]}`)
+}
