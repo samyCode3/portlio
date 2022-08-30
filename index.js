98bd804e407812fcee37a6a578410ecc2c0ccd55
samson onifade <107084481+samyCode3@users.noreply.github.com>
2022-08-31 00:20:37 +0100

    Update

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-const slider = document.getElementById('image-slider')
-const images = ['3d-interlocked-hands-four-people-27283315.jpg',
-'code1.png', 'code2.png', 'code3.png', 'code4.png'
-]
+// const slider = document.getElementById('image-slider')
+// const images = ['3d-interlocked-hands-four-people-27283315.jpg',
+// 'code1.png', 'code2.png', 'code3.png', 'code4.png'
+// ]
 
-let i = 0;
-function prev()
-{
-     if(i <= 0) i = images.length
-     i--
-     return setImg();
-}
+// let i = 0;
+// function prev()
+// {
+//      if(i <= 0) i = images.length
+//      i--
+//      return setImg();
+// }
 
-function next()
-{
- if(i >= images.length-1) i = -1;
- i++ 
- return setImg()
-}
+// function next()
+// {
+//  if(i >= images.length-1) i = -1;
+//  i++ 
+//  return setImg()
+// }
 
-function setImg( )
-{
-     return slider.setAttribute('src', `./${images[i]}`)
-}
+// function setImg( )
+// {
+//      return slider.setAttribute('src', `./${images[i]}`)
+// }
